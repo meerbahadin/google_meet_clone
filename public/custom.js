ac61d47aab1f054353703ac16bce1d67d8b58624
meer bahadin <you@example.com>
2020-08-29 11:51:16 +0300

    v2 added some controls

--- public/custom.js
+++ public/custom.js
@@ -6,6 +6,7 @@ const myPeer = new Peer(undefined , {
   port: 443,
 });
 const myVideo = document.createElement('video');
+let mystream;
 myVideo.muted = true;
 const peers = {};
 navigator.mediaDevices
@@ -14,8 +15,8 @@ navigator.mediaDevices
     audio: true,
   })
   .then((stream) => {
+    mystream = stream;
     addVideoStream(myVideo, stream);
-
     myPeer.on('call', (call) => {
       call.answer(stream);
       const video = document.createElement('video');
@@ -50,10 +51,67 @@ function connectToNewUser(userId, stream) {
   peers[userId] = call;
 }
 
-function addVideoStream(video, stream) {
+function addVideoStream(video , stream) {
   video.srcObject = stream;
   video.addEventListener('loadedmetadata', () => {
     video.play();
   });
   videoGrid.append(video);
+}
+
+
+const miceToggle = () => {
+  const enabled = mystream.getAudioTracks()[0].enabled;
+  if (enabled) {
+    mystream.getAudioTracks()[0].enabled = false;
+    console.log('success');
+    setUnmuteButton();
+  } else {
+    setMuteButton();
+    mystream.getAudioTracks()[0].enabled = true;
+  }
+}
+
+const setMuteButton = () => {
+  const html = `
+    <i class="fas fa-microphone"></i>
+    <span>Mute</span>
+  `
+  document.querySelector('#mic').innerHTML = html;
+}
+
+const setUnmuteButton = () => {
+  const html = `
+    <i class="unmute fas fa-microphone-slash"></i>
+    <span>Unmute</span>
+  `
+  document.querySelector('#mic').innerHTML = html;
+}
+
+const videoToggle = () => {
+  console.log('object')
+  let enabled = mystream.getVideoTracks()[0].enabled;
+  if (enabled) {
+    mystream.getVideoTracks()[0].enabled = false;
+    setPlayVideo()
+  } else {
+    setStopVideo()
+    mystream.getVideoTracks()[0].enabled = true;
+  }
+}
+
+const setStopVideo = () => {
+  const html = `
+    <i class="fas fa-video"></i>
+    <span>Stop Video</span>
+  `
+  document.querySelector('#camera').innerHTML = html;
+}
+
+const setPlayVideo = () => {
+  const html = `
+  <i class="stop fas fa-video-slash"></i>
+    <span>Play Video</span>
+  `
+  document.querySelector('#camera').innerHTML = html;
 }
